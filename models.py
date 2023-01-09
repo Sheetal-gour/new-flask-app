957b2a13bbdd6ac914c669d135cf7d7d5236232f
Sheetal Gour <sgour@deqode.com>
2023-01-09 16:48:25 +0530

    Comments deleted

--- models.py
+++ models.py
@@ -1,23 +1,3 @@
-# from flask_sqlalchemy import SQLAlchemy
-
-
-# db = SQLAlchemy()
-
-
-
-# class Employee(db.Model):
-#     __tablename__ = 'results'
-
-#     name = db.Column(db.String(30),primary_key = True)
-#     age = db.Column(db.Integer())
-
-#     def __init__(self, name,age):
-#         self.name = name
-#         self.age  = age
-        
-#     def __repr__(self):
-#         return '<id {self.}>'.format(self.name)
-
 from flask_sqlalchemy import SQLAlchemy
 
 # app = Flask(__name__)
